cbc9515554ceacb1576e3613387f9dd402dc7b2a
ShubhamShekade30 <shubhamshekade.ss80@gmail.com>
2024-12-29 22:55:53 +0530

    HRM AND Telecom

---
+++ Human-Resource-Management/Scenario2/LoginTest.java
@@ -0,0 +1,64 @@
+package com.Scenario2;
+
+import org.testng.annotations.Test;
+
+import Scenario2Utility.Propertiesreader;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+public class LoginTest {
+	WebDriver driver;
+	LoginClass login=new LoginClass(driver);
+	Propertiesreader prop;
+	AdminClass ad=new AdminClass(driver);
+	
+  
+
+  @BeforeTest
+  public void beforeTest() {
+	  prop=new Propertiesreader();
+	  driver=new ChromeDriver();
+	  driver.get(prop.getData("url"));
+	  login.addWait();
+	  driver.manage().window().maximize();
+	  login=new LoginClass(driver);
+  }
+  
+       @Test(priority=1)
+        public void loginTest()
+       {
+    	   login.login(prop.getData("username"), prop.getData("password"));
+    	   
+       }
+       
+       @Test(priority=2)
+       public void validateUrl()
+       {
+    	   String url=login.appUrl();
+    	   Assert.assertEquals(url,"https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index", "Url not matched. Test failed ");
+    	   System.out.println("Url Matched launch success. Test Passed");
+       }
+       
+       
+       @Test(priority=3)
+       public void validateTitle()
+       {
+    	   System.out.println("Page Tilte: "+login.appTitle());
+    	   Assert.assertEquals(login.appTitle().contains("OrangeHRM"),true, "Title not matched. Test failed");
+    	   System.out.println("Title matched launch success.  Test passed");
+       }
+       
+       
+     @AfterClass
+     public void tearDown() {
+    	 if(driver!=null)
+    		 driver.quit();
+    	 else
+    		 System.out.println("Driver is null");
+     }
+
+     
+}
